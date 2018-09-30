11e340af96584411991bd2542840429773a387a9
DarkNin <darknin@outlook.com>
2018-09-30 16:07:01 +0800

    uw front end 1.0.2提测存档

--- UW_FRONTEND/src/pages/material/details/comp/subscomp/AddMaterial.vue
+++ UW_FRONTEND/src/pages/material/details/comp/subscomp/AddMaterial.vue
@@ -10,6 +10,7 @@
         <div class="form-row col-4 pl-2 pr-2">
           <label for="material-no" class="col-form-label">料号:</label>
           <input type="text" id="material-no" class="form-control" v-model="thisData.no"  autocomplete="off">
+          <span class="form-span col"></span>
         </div>
         <div class="form-row col-4 pl-2 pr-2">
           <label for="material-area" class="col-form-label">区域:</label>
