44a3226b990c2ed4766531b72e576d5fb6a50786
HardyYao <hardyyao1995@qq.com>
2018-11-05 10:20:51 +0800

    完善代码：   4）修改 nextRound方法 的判断逻辑，针对 isForceFinish字段 为 false 的任务条目，不管是出库任务还是入库任务，任务条目状态都要回滚到0

--- uw_server/src/main/java/com/jimi/uw_server/agv/handle/LSSLHandler.java
+++ uw_server/src/main/java/com/jimi/uw_server/agv/handle/LSSLHandler.java
@@ -116,14 +116,12 @@ public class LSSLHandler {
 	}
 
 
+	// 将任务条目状态回滚到0
 	private static void nextRound(AGVIOTaskItem item) {
-		// 如果是出库任务，若计划出库数量小于实际出库数量，则将任务条目状态回滚到0
-		if (Task.dao.findById(item.getTaskId()).getType() == 1) {
-			if (!item.getIsForceFinish()) {
-				TaskItemRedisDAO.updateTaskItemState(item, 0);
-				TaskItemRedisDAO.updateTaskItemRobot(item, 0);
-				TaskItemRedisDAO.updateTaskItemBoxId(item, 0);
-			}
+		if (!item.getIsForceFinish()) {
+			TaskItemRedisDAO.updateTaskItemState(item, 0);
+			TaskItemRedisDAO.updateTaskItemRobot(item, 0);
+			TaskItemRedisDAO.updateTaskItemBoxId(item, 0);
 		}
 	}
 
