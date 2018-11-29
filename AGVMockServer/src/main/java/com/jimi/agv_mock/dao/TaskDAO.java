240c6054682ec0f7862aa39a6793536ef7c7f94a
HardyYao <hardyyao1995@qq.com>
2018-11-29 10:01:02 +0800

    添加AGVMockServer测试环境数据库配置信息

--- AGVMockServer/src/main/java/com/jimi/agv_mock/dao/TaskDAO.java
+++ AGVMockServer/src/main/java/com/jimi/agv_mock/dao/TaskDAO.java
@@ -1,5 +1,7 @@
 package com.jimi.agv_mock.dao;
 
+import java.io.File;
+
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
 import com.jfinal.plugin.activerecord.dialect.MysqlDialect;
 import com.jfinal.plugin.druid.DruidPlugin;
@@ -13,12 +15,19 @@ import com.jfinal.plugin.druid.DruidPlugin;
 public class TaskDAO {
 
 	private static final String URL = "jdbc:mysql://localhost:3306/uw?characterEncoding=utf8";
-	private static final String USER = "root";
-	private static final String PASSWORD = "newttl!@#$1234";
+	private static final String D_USER = "root";
+	private static final String D_PASSWORD = "newttl!@#$1234";
+	private static final String T_USER = "standard";
+	private static final String T_PASSWORD = "Jimisql#1234";
 	
 	
 	public static void init() {
-		DruidPlugin dp = new DruidPlugin(URL, USER, PASSWORD);
+		DruidPlugin dp = null;
+		if (isTestEnvironment()) {
+			dp = new DruidPlugin(URL, T_USER, T_PASSWORD);
+		} else {
+			dp = new DruidPlugin(URL, D_USER, D_PASSWORD);
+		}
 		dp.start();
 		ActiveRecordPlugin arp = new ActiveRecordPlugin(dp);
 	    arp.setDialect(new MysqlDialect());
@@ -30,4 +39,16 @@ public class TaskDAO {
 	public static int getWindowId(int taskId) {
 		return Task.dao.findById(taskId).getWindow();
 	}
+
+
+	public static boolean isTestEnvironment() {
+		File[] roots = File.listRoots();
+        for (int i=0; i < roots.length; i++) {
+            if(new File(roots[i].toString() + "TEST_ENVIRONMENT_FLAG").exists()) {
+            	return true;
+            }
+        }
+        return false;
+	}
+
 }
